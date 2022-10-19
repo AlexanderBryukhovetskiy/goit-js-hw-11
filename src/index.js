d6eb78ca204ad361ea92249684436b878d4b1df9
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2022-10-19 21:03:18 +0300

    start-files copied; base html+css added;
    +refs.js created & imported at index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// all modules
+import Notiflix from 'notiflix';
+
+import {refs} from './js/refs';
+
+
+
+console.log(refs);
+console.log(refs.searchForm);
+console.log(refs.input);
+console.log(refs.submitBtn);
+console.log(refs.list);
+console.log(refs.guarder);
---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+
+export const refs = {
+    searchForm : document.querySelector('#search-form'),
+    input : document.querySelector('input[name="searchQuery"]'),
+    submitBtn : document.querySelector('button[type="submit"]'),
+    list : document.querySelector('.list'),
+    guarder : document.querySelector('.guarder')
+}
