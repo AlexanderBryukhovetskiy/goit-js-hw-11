65948ee2cf68b4f84dfd8cdb3e428a7185a913a4
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2022-10-19 22:03:37 +0300

    js files added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 // all modules
 import Notiflix from 'notiflix';
+import simpleLightbox from 'simplelightbox';
 
 import {refs} from './js/refs';
 
+// console.log(refs);
+// console.log(refs.searchForm);
+// console.log(refs.input);
+// console.log(refs.submitBtn);
+// console.log(refs.list);
+// console.log(refs.guarder);
 
+refs.searchForm.addEventListener('submit', onSearch);
 
-console.log(refs);
-console.log(refs.searchForm);
-console.log(refs.input);
-console.log(refs.submitBtn);
-console.log(refs.list);
-console.log(refs.guarder);
+function onSearch (e) { 
+    e.preventDefault();
+
+    console.log("submit is work!");
+}
