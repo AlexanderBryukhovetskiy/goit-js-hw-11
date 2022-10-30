5474c60aa3c40abb293121e30f5b930b9980a9c0
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2022-10-30 21:52:04 +0300

    Update searchFunction.js

--- src/js/searchFunction.js
+++ src/js/searchFunction.js
@@ -12,7 +12,9 @@ export async function getPictures(searchName) {
    
     if (searchName === '') { 
         Notiflix.Notify.info('Sorry, search query can not be empty. Please try again.', emptyMarkup());
+
         refs.input.value = "";
+        localStorage.setItem('searchName', '');
         refs.loadMoreBtn.hidden = true;
         return
     }
@@ -20,13 +22,11 @@ export async function getPictures(searchName) {
     const response = await axios.get(`${URL}?key=${KEY}&q=${searchName}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`);
         
     if ( response.data.hits <= 0 || response.status === 404) {
-        
-
         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.', emptyMarkup());
         
         refs.loadMoreBtn.hidden = true;
-
         refs.input.value = "";
+        localStorage.setItem('searchName', '');
         
         throw new Error();
     }
